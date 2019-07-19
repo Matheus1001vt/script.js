f7a813df80a271a98d41805f51dbfd86e440ffc3
Matheus <matheus24204@gmail.com>
2019-07-19 17:11:33 -0300

    Adicionar trabalho de script.js

---
+++ script.js
@@ -0,0 +1,49 @@
+let dia = Number(prompt("Calendário semanal"))
+
+if (dia == 1) {
+    alert("domingo")
+}
+
+else if (dia == 2){
+    alert("segunda")
+}
+
+else if (dia == 3){
+    alert("terça")
+}
+
+else if (dia == 4){
+    alert("quarta")
+}
+
+else if (dia == 5){
+    alert("quinta")
+}
+
+else if (dia == 6){
+    alert("sexta")
+}
+
+else if (dia == 7){
+    alert("sabado")
+}
+
+
+
+
+
+
+
+
+
+
+
+// if (){
+
+// }
+// else if () {
+
+//}
+//else {
+
+//}
